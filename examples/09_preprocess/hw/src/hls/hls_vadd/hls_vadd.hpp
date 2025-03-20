16df2209b66823a84092ce4caddd9abe2809c6b1
Arith2 <1787753693@qq.com>
2025-03-20 22:29:37 +0100

    Add 09_preprocess for DLRM prerpocessing tasks

--- examples/09_preprocess/hw/src/hls/hls_vadd/hls_vadd.hpp
+++ examples/09_preprocess/hw/src/hls/hls_vadd/hls_vadd.hpp
@@ -1,5 +1,8 @@
 #include "hls_stream.h"
 #include "ap_axi_sdata.h"
+#include <stdint.h>
+#include <ap_int.h>
+#include <hls_math.h>
 
 // Constants and typedefs
 #define AXI_DATA_BITS 512
@@ -8,19 +11,155 @@ typedef ap_axiu<AXI_DATA_BITS, 0, 0, 0> axi_s;
 #define FLOAT_BITS 32
 #define NUM_FLOATS AXI_DATA_BITS / FLOAT_BITS
 
+typedef union
+{
+    float float32;
+    uint32_t uint32;
+} conv;
+
 /**
  * hls_vadd example
  * @brief Reads floats from the two incoming streams and addes them, storing the result to axi_out
  * 
- * @param[in] axi_in1 Incoming AXI stream, corresponding to vector a
- * @param[in] axi_in2 Incoming AXI stream, corresponding to vector b
+ * @param[in] axi_in Incoming AXI stream, corresponding to vector
  * @param[out] axi_out Outgoing AXI stream; resulting vector
  *
  * The inputs/outputs are actually buffered in a hls::stream, which corresponds to a FIFO
  * Each element in the FIFO is a AXI stream with data, valid etc. signals
  */
 void hls_vadd (
-    hls::stream<axi_s> &axi_in1,
-    hls::stream<axi_s> &axi_in2,
+    hls::stream<axi_s> &axi_in,
     hls::stream<axi_s> &axi_out
 );
+
+void LoadData(
+    hls::stream<axi_s> &axi_in, 
+    hls::stream<ap_uint<512> >& stream_out 
+) {
+#pragma HLS PIPELINE II=1
+#pragma HLS INLINE off
+
+        axi_s x;
+        ap_uint<512> y;
+
+        if(!axi_in.empty()) {
+            x = axi_in.read();
+            y = x.data;
+            stream_out.write(y);
+        }
+
+}
+
+void Dense_NegsToZero(
+    hls::stream<ap_uint<512> >& stream_in,
+    hls::stream<ap_uint<512> >& stream_out 
+) {
+#pragma HLS PIPELINE II=1
+#pragma HLS INLINE off
+
+        ap_uint<512> input_data;
+        ap_uint<512> output_data;
+
+        Dense_NegsToZero:
+        if(!stream_in.empty()) {
+
+            input_data = stream_in.read();
+
+            for (int k = 0; k < 16; k++) {
+                #pragma HLS unroll
+
+                int tmp_value;
+                conv conv_value;
+
+                tmp_value = input_data(32*k+31, 32*k);
+                tmp_value = (tmp_value < 0) ? 0 : tmp_value;
+                output_data(32*k+31, 32*k) = tmp_value;
+
+            }
+
+			stream_out.write(output_data);
+		}
+
+}
+
+void Dense_Log(
+    hls::stream<ap_uint<512> >& stream_in,
+    hls::stream<ap_uint<512> >& stream_out 
+) {
+#pragma HLS PIPELINE II=1
+#pragma HLS INLINE off
+
+
+    ap_uint<512> input_data = 0;
+    ap_uint<512> output_data = 0;
+
+    Dense_Log:
+    if(!stream_in.empty()) {
+
+        input_data = stream_in.read();
+
+        for (int j = 0; j < 16; j++) {
+            #pragma HLS unroll
+
+            int tmp_value;
+            conv conv_value;
+
+            tmp_value = input_data(32*j+31, 32*j);
+            conv_value.float32 = hls::logf(tmp_value+1);
+            output_data(32*j+31, 32*j) = conv_value.uint32; 
+
+        }
+
+		stream_out.write(output_data);
+	}
+
+}
+
+void Sparse_HexToIntMod(
+    hls::stream<ap_uint<512> >& stream_in,
+    hls::stream<ap_uint<512> >& stream_out
+) {
+#pragma HLS PIPELINE II=1
+#pragma HLS INLINE off
+
+    ap_uint<512> input_data = 0;
+    ap_uint<512> output_data = 0;
+
+    Sparse_HexToIntMod:
+    if(!stream_in.empty()) {
+
+        input_data = stream_in.read();
+
+        for (int j = 0; j < 16; j++) {
+            #pragma HLS unroll
+
+            ap_uint<32> tmp_input = input_data(32*j+31, 32*j);
+            ap_uint<32> tmp_output = tmp_input & 0x3FF; // % 1024
+            output_data(32*j+31, 32*j) = tmp_output; 
+
+        }
+
+		stream_out.write(output_data);
+	}
+}
+
+void StoreData(
+    hls::stream<ap_uint<512> >& stream_in,
+    hls::stream<axi_s> &axi_out
+) {
+#pragma HLS PIPELINE II=1
+#pragma HLS INLINE off
+
+    // ap_uint<512> input_data = 0;
+    axi_s y;
+
+    StoreData:
+    if(!stream_in.empty()) {
+
+		y.data = stream_in.read();
+		y.keep = 0xF;
+		y.id = 0;
+		y.last = 0;
+		axi_out.write(y);
+	}
+}
--- examples/09_preprocess/hw/src/vfpga_top.svh
+++ examples/09_preprocess/hw/src/vfpga_top.svh
@@ -1,18 +1,18 @@
 `ifdef EN_STRM
 hls_vadd inst_vadd(
-    .s_axi_in1_TDATA        (axis_host_recv[0].tdata),
-    .s_axi_in1_TKEEP        (axis_host_recv[0].tkeep),
-    .s_axi_in1_TLAST        (axis_host_recv[0].tlast),
-    .s_axi_in1_TSTRB        (0),
-    .s_axi_in1_TVALID       (axis_host_recv[0].tvalid),
-    .s_axi_in1_TREADY       (axis_host_recv[0].tready),
+    .s_axi_in_TDATA        (axis_host_recv[0].tdata),
+    .s_axi_in_TKEEP        (axis_host_recv[0].tkeep),
+    .s_axi_in_TLAST        (axis_host_recv[0].tlast),
+    .s_axi_in_TSTRB        (0),
+    .s_axi_in_TVALID       (axis_host_recv[0].tvalid),
+    .s_axi_in_TREADY       (axis_host_recv[0].tready),
 
-    .s_axi_in2_TDATA        (axis_host_recv[1].tdata),
-    .s_axi_in2_TKEEP        (axis_host_recv[1].tkeep),
-    .s_axi_in2_TLAST        (axis_host_recv[1].tlast),
-    .s_axi_in2_TSTRB        (0),
-    .s_axi_in2_TVALID       (axis_host_recv[1].tvalid),
-    .s_axi_in2_TREADY       (axis_host_recv[1].tready),
+    // .s_axi_in2_TDATA        (axis_host_recv[1].tdata),
+    // .s_axi_in2_TKEEP        (axis_host_recv[1].tkeep),
+    // .s_axi_in2_TLAST        (axis_host_recv[1].tlast),
+    // .s_axi_in2_TSTRB        (0),
+    // .s_axi_in2_TVALID       (axis_host_recv[1].tvalid),
+    // .s_axi_in2_TREADY       (axis_host_recv[1].tready),
 
     .m_axi_out_TDATA        (axis_host_send[0].tdata),
     .m_axi_out_TKEEP        (axis_host_send[0].tkeep),
@@ -27,7 +27,7 @@ hls_vadd inst_vadd(
 
 // There are two host streams, for both incoming and outgoing signals
 // The second outgoing is unused in this example, so tie it off
-always_comb axis_host_send[1].tie_off_m();
+// always_comb axis_host_send[1].tie_off_m();
 `endif
 
 // Tie-off unused signals to avoid synthesis problems
@@ -48,21 +48,22 @@ However, in the IP instantiation (```init_ip.tcl```), the ILA IP is defined as `
 So the mismatch will cause synthesis errors.
 Therefore, whenever possible the HLS kernel name should only be contained in the HLS kernel IP and in no other IP names/instances.
 */
-ila_vadd inst_ila_vadd (
-    .clk(aclk),                             // clock   
+
+// ila_vadd inst_ila_vadd (
+//     .clk(aclk),                             // clock   
  
-    .probe0(axis_host_recv[0].tvalid),      // 1
-    .probe1(axis_host_recv[0].tready),      // 1
-    .probe2(axis_host_recv[0].tlast),       // 1
-    .probe3(axis_host_recv[0].tdata),       // 512
+//     .probe0(axis_host_recv[0].tvalid),      // 1
+//     .probe1(axis_host_recv[0].tready),      // 1
+//     .probe2(axis_host_recv[0].tlast),       // 1
+//     .probe3(axis_host_recv[0].tdata),       // 512
 
-    .probe4(axis_host_recv[1].tvalid),      // 1
-    .probe5(axis_host_recv[1].tready),      // 1
-    .probe6(axis_host_recv[1].tlast),       // 1
-    .probe7(axis_host_send[1].tdata),       // 512
+//     .probe4(axis_host_recv[1].tvalid),      // 1
+//     .probe5(axis_host_recv[1].tready),      // 1
+//     .probe6(axis_host_recv[1].tlast),       // 1
+//     .probe7(axis_host_send[1].tdata),       // 512
 
-    .probe8(axis_host_send[0].tvalid),      // 1
-    .probe9(axis_host_send[0].tready),      // 1
-    .probe10(axis_host_send[0].tlast),      // 1
-    .probe11(axis_host_send[0].tdata)       // 512
-);
+//     .probe8(axis_host_send[0].tvalid),      // 1
+//     .probe9(axis_host_send[0].tready),      // 1
+//     .probe10(axis_host_send[0].tlast),      // 1
+//     .probe11(axis_host_send[0].tdata)       // 512
+// );
